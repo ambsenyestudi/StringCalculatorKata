e7efe82635d4e5071760ce924f5cc0eb8fd68651
Adrià Freixas <adrifre7@hotmail.com>
2018-02-15 18:00:33 +0100

    First part attempt

--- StringCalculator/StringCalculator.UT/AddUT.cs
+++ StringCalculator/StringCalculator.UT/AddUT.cs
@@ -16,8 +16,57 @@ namespace StringCalculator.UT
         public void GivenSEmptyShouldBe0()
         {
             var expected = 0;
-            //Act
-            Assert.IsTrue(true);
+            var result = sut.Add(string.Empty);
+            Assert.IsTrue(result == expected);
+        }
+        [TestMethod]
+        public void Given1ShouldBe1()
+        {
+            var expected = 1;
+            var result = sut.Add("1");
+            Assert.IsTrue(result == expected);
+        }
+        [TestMethod]
+        public void Given2NumberShouldBeSum()
+        {
+            var expected = 3;
+            var result = sut.Add("1,2");
+            Assert.IsTrue(result == expected);
+        }
+        [TestMethod]
+        public void GivenNewLineNumberShouldBeSum()
+        {
+            var expected = 6;
+            var result = sut.Add("1\n2,3");
+            Assert.IsTrue(result == expected);
+        }
+        [TestMethod]
+        public void GivenDelemitresNumberShouldBeSum()
+        {
+            var expected = 3;
+            var result = sut.Add("//;\n1;2");
+            Assert.IsTrue(result == expected);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(FormatException),
+            "Negatives not allowed -1")]
+        public void GivenNegative1NumberShouldFormatException()
+        {
+            var result = sut.Add("-1");
+        }
+        [TestMethod]
+        [ExpectedException(typeof(FormatException),
+            "Negatives not allowed -256")]
+        public void GivenNegative256And1NumberShouldFormatException()
+        {
+            var result = sut.Add("-256,1");
+        }
+        [TestMethod]
+        [ExpectedException(typeof(FormatException),
+            "Negatives not allowed -256, -128")]
+        public void GivenNegative256And1AndNeg128NumberShouldFormatException()
+        {
+            var result = sut.Add("-256,1,-128");
         }
     }
 }
--- StringCalculator/StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator/StringCalculator.cs
@@ -8,5 +8,72 @@ namespace StringCalculator
 {
     public class StringCalculator
     {
+        readonly List<char> addmitedSeparators = new List<char>{ ',', '\n' };
+        char defaultSeparator = ',';
+        public int Add(string numbers)
+        {
+            var result = 0;
+            if(numbers.Length>0)
+            {
+                CheckForNegatives(numbers);
+                if (numbers.Length < 2)
+                {
+                    result = int.Parse(numbers);
+                }
+                else
+                {
+                    result = AddAnyAmmountofNumbers(numbers);
+                }
+            }
+            return result;
+        }
+        private void CheckForNegatives(string numbers)
+        {
+            var negSeparator = '-';
+            if (numbers.Contains(negSeparator))
+            {
+                numbers = NormalizeSeparators(numbers);
+                string[] nums = numbers.Split(defaultSeparator);
+                var negNumb = from num in nums where num.Contains(negSeparator) select num;
+                string negNumbers = string.Join(", ", negNumb.ToList<string>());
+                var msg = "Negatives not allowed " + negNumbers;
+                throw new FormatException(msg);
+            }
+        }
+
+        private int AddAnyAmmountofNumbers(string numbers)
+        {
+            var result = 0;
+            numbers = NormalizeSeparators(numbers);
+            if (numbers.Length > 0)
+            {
+                int currResult;
+                string[] nums = numbers.Split(defaultSeparator);
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    if (int.TryParse(nums[i], out currResult))
+                    {
+                        result += currResult;
+                    }
+                }
+            }
+            return result;
+        }
+        private string NormalizeSeparators(string numbers)
+        {
+            defaultSeparator = addmitedSeparators[0];
+            
+            if (numbers.Contains('/'))
+            {
+                defaultSeparator = numbers[numbers.LastIndexOf('/') + 1];
+                numbers = numbers.Substring(numbers.IndexOf(defaultSeparator)+1);
+            }
+            
+            foreach(var sep in addmitedSeparators)
+            {
+                numbers=numbers.Replace(sep, defaultSeparator);
+            }
+            return numbers;
+        }
     }
 }
