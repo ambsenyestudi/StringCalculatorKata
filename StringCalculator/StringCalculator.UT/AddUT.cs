068ccbab0364169d98f562fe7c245e2ceffb2f97
Adrià Freixas <adrifre7@hotmail.com>
2018-02-15 17:03:31 +0100

    Starter

---
+++ StringCalculator/StringCalculator.UT/AddUT.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringCalculator.UT
+{
+    [TestClass]
+    public class AddUT
+    {
+        StringCalculator sut;
+        [TestInitialize]
+        public void InitSUT()
+        {
+            sut = new StringCalculator();
+        }
+        [TestMethod]
+        public void GivenSEmptyShouldBe0()
+        {
+            var expected = 0;
+            //Act
+            Assert.IsTrue(true);
+        }
+    }
+}
---
+++ StringCalculator/StringCalculator.UT/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StringCalculator.UT")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StringCalculator.UT")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2b177697-903d-408f-b3d4-090fe971e349")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
